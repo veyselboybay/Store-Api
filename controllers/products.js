3f217f6215b7f915872f05c83bf99d1c829f5eb1
veyselboybay <veysel.boybay@outlook.com>
2022-09-16 12:54:11 -0400

    working on sort with queries in mongodb

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,25 @@
-
+const Product = require('../models/product');
 
 const  getAllProductsStatic = async (req,res) => {
-    throw new Error('testing async error');
-    res.status(200).json({msg:'products testing route'})
+    const products = await Product.find({}).sort('-name price');
+    res.status(200).json({products, nbHits: products.length })
 }
 
 const  getAllProducts = async (req,res) => {
-    res.status(200).json({msg:'products testing route'})
+    const { featured, company, name, sort } = req.query;
+    const queryObject = {};
+
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if(company){
+        queryObject.company = {$regex:company, $options:'i'};
+    }
+    if(name){
+        queryObject.name = {$regex:name, $options:'i'};
+    }
+    const products = await Product.find(queryObject);
+    res.status(200).json({products, nbHits: products.length })
 }
 
 module.exports = {
