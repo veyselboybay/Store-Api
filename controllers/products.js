18cc8ddd7828d0f6533c46958efd3c6dcba04dca
veyselboybay <veysel.boybay@outlook.com>
2022-09-18 16:49:24 -0400

    sorting functionality added to the API and completed the api project

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const  getAllProductsStatic = async (req,res) => {
 }
 
 const  getAllProducts = async (req,res) => {
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
     const queryObject = {};
 
     if(featured){
@@ -18,7 +18,44 @@ const  getAllProducts = async (req,res) => {
     if(name){
         queryObject.name = {$regex:name, $options:'i'};
     }
-    const products = await Product.find(queryObject);
+    if(numericFilters){
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+        }
+        const regEx = /\b(>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx,(match) => `-${operatorMap[match]}-`);
+        const options = ['price','rating'];
+        filters = filters.split(',').forEach(item => {
+            const [field,operator,value] = item.split('-');
+            if(options.includes(field)){
+                queryObject[field] = {[operator]:Number(value)};
+            }
+        });
+    }
+    console.log(queryObject);
+    let result = Product.find(queryObject);
+
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }else{
+        result = result.sort('createdAt');
+    }
+    if(fields){
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page-1)*limit;
+
+    result = result.skip(skip).limit(limit);
+    const products = await result;
     res.status(200).json({products, nbHits: products.length })
 }
 
